968f913d09930112e6a3d8d64b34c7074da82111
kazitishan <kazitishan12@gmail.com>
2026-05-17 03:50:38 -0400

    Make sure that private/data/info.json will be there when running npm run dev or npm run build

---
+++ scripts/setup.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataDir = path.join(__dirname, "..", "private", "data");
+const infoPath = path.join(dataDir, "info.json");
+
+if (fs.existsSync(infoPath)) process.exit(0);
+
+const skeleton = {
+  "personal info": {
+    "first name": "",
+    "last name": "",
+    "contact info": {
+      email: "",
+      "country code": "",
+      phone: "",
+      linkedIn: "",
+      website: "",
+      github: "",
+    },
+    address: {
+      street: "",
+      apt: "",
+      city: "",
+      state: "",
+      "zip code": "",
+      country: "",
+      county: "",
+    },
+  },
+  "school info": [
+    {
+      "school name": "",
+      degree: "",
+      major: "",
+      "start month": "",
+      "start year": "",
+      "graduation month": "",
+      "graduation year": "",
+    },
+  ],
+  experience: [
+    {
+      "job title": "",
+      company: "",
+      location: "",
+      "current job": false,
+      "start month": "",
+      "start year": "",
+      "end month": null,
+      "end year": null,
+      "role description": [],
+    },
+  ],
+  languages: [
+    {
+      language: "",
+      fluent: false,
+      reading: "",
+      "reading / speaking / writing": "",
+      speaking: "",
+      writing: "",
+      proficiency: "",
+    },
+  ],
+  attachments: {
+    resume: "",
+    "cover letter": "",
+  },
+  "equal employment opportunity info": {
+    gender: "",
+    ethnicity: "",
+    race: "",
+    "veteran status": "",
+    "disability status": "",
+  },
+  "work authorization": {
+    "Are you currently authorized to work in the United States?": "",
+    "Will you now or in the future require sponsorship for employment visa status (e.g., H-1B visa status)?": "",
+    citizenship: "",
+  },
+};
+
+fs.mkdirSync(dataDir, { recursive: true });
+fs.writeFileSync(infoPath, JSON.stringify(skeleton, null, 4), "utf-8");
+console.log("Created private/data/info.json");
